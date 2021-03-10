ec542ebc0787683e11804f7c7b37d11bc6990a75
mmskazak <mmskazak@gmail.com>
2021-03-10 17:35:22 +0300

    first version

---
+++ SayHello.php
@@ -0,0 +1,14 @@
+<?php
+
+class SayHello {
+
+    public function ru(): string
+    {
+        return 'Привет';    
+    }
+
+    public function en(): string
+    {
+        return 'Hello';    
+    }
+}
